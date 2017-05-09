b775b52ad4bdb18836cc97bc339ac1a3d05022d4
fredde <frerik@ginsberg.se>
2017-05-09 09:28:19 +0200

    v2

--- main.c
+++ main.c
@@ -2,23 +2,36 @@
     Simple udp server
 */
 #include<stdio.h> //printf
+#include<stdint.h>
 #include<string.h> //memset
 #include<stdlib.h> //exit(0);
 #include<arpa/inet.h>
 #include<sys/socket.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 
 #define BUFLEN 512  //Max length of buffer
 #define PORT 8888   //The port on which to listen for incoming data
 
 #define MAXDATA 200
+#define INITCONNECT 0
+#define WAITINITCONNECT 1
+#define WAITINGCONFIRMCONNECT 2
+#define CONNECTED 3
+#define INITCLOSE 4
+#define WAITINGCLOSE 5
+#define CLOSED 6
 
 typedef struct {
 
-    int seq;
-    int ack;
-    char data[MAXDATA];
+    bool fin;
+    bool reset;
+    uint64_t seq;
+    uint64_t ack;
+    uint16_t timeStamp;
+    char data;
+    uint64_t checkSum;
 } Package;
 
 
@@ -30,31 +43,91 @@ void die(char *s)
 
 int main(void)
 {
-    struct sockaddr_in si_me, si_other, AcceptedHost;
+    struct sockaddr_in serverInfo, clientInfo, AcceptedHost;
     Package buf;
+    fd_set activeFdSet, readFdSet;
 
-    int s, i, slen = sizeof(si_other) , recv_len;
+    int sock, i, slen = sizeof(clientInfo) , recv_len;
     //char buf[BUFLEN];
 
     //create a UDP socket
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    if ((sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
         die("socket");
     }
 
     // zero out the structure
-    memset((char *) &si_me, 0, sizeof(si_me));
+    memset((char *) &serverInfo, 0, sizeof(serverInfo));
 
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(PORT);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverInfo.sin_family = AF_INET;
+    serverInfo.sin_port = htons(PORT);
+    serverInfo.sin_addr.s_addr = htonl(INADDR_ANY);
 
     //bind socket to port
-    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+    if( bind(sock , (struct sockaddr*)&serverInfo, sizeof(serverInfo) ) == -1)
     {
         die("bind");
     }
 
+    /* Initialize the set of active sockets */
+    FD_ZERO(&activeFdSet); // initializes an empty FDset
+    FD_SET(sock, &activeFdSet); // puts socket in the set, activeFDset currently only contains this socket.
+
+int currentState = INITCONNECT;
+
+    while(1) {
+        switch (currentState) {
+            case INITCONNECT:
+                readFdSet = activeFdSet;
+
+
+                if (select(FD_SETSIZE, &readFdSet, NULL, NULL, NULL) < 0) { // blocking, waits until one the FD is set to ready, will keep the ready ones in readFdSet
+                    perror("Select failed\n");
+                    exit(EXIT_FAILURE);
+                }
+                else if(FD_ISSET(sock, &readFdSet))
+                {
+                    if ((recv_len = recvfrom(sock, &buf, sizeof(Package), 0, (struct sockaddr *) &clientInfo, &slen)) == -1) {
+                        // perror(recvfrom());
+                        die("recvfrom()");
+                    }
+
+
+                    // break
+                }
+                else
+                {
+                    // want to quit?
+                     break;
+
+                }
+                //currentState = WAITINITCONNECT;
+
+                break;
+            case WAITINITCONNECT:
+
+
+                break;
+            case WAITINGCONFIRMCONNECT:
+
+                break;
+
+            case CONNECTED:
+
+                break;
+            case INITCLOSE:
+
+                break;
+            case WAITINGCLOSE:
+
+                break;
+            case CLOSED:
+
+                break;
+
+        }
+    }
+/*
     //keep listening for data
     while(1)
     {
@@ -62,24 +135,26 @@ int main(void)
         fflush(stdout);
 
         //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(s, &buf, sizeof(Package), 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        if ((recv_len = recvfrom(s, &buf, sizeof(Package), 0, (struct sockaddr *) &clientInfo, &slen)) == -1)
         {
+           // perror(recvfrom());
             die("recvfrom()");
         }
 
 
         //print details of the client/peer and the data received
-        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        printf("Received packet from %s:%d\n", inet_ntoa(clientInfo.sin_addr), ntohs(clientInfo.sin_port));
         printf("Data: %d %d %s\n" , buf.seq, buf.ack, buf.data);
 
 
         //now reply the client with the same data
-        if (sendto(s, &buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
+        if (sendto(s, &buf, sizeof(Package), 0, (struct sockaddr*) &clientInfo, slen) == -1)
         {
             die("sendto()");
         }
     }
 
     close(s);
+    */
     return 0;
 }
